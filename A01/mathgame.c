f3b43c8f4293967463d50a84a35de3bf9ccfed32
Lina Luo <qluo1@haverford.edu>
2025-01-21 16:00:57 -0500

    Descriptive message

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -8,5 +8,21 @@
 
 int main() {
   printf("Welcome to Math Game!");
+  int round = scanf("How many rounds do you want to play?");
+  int correct = 0;
+  for (int i = 0; i < round; i++) {
+    int num1 = rand();
+    int num2 = rand();
+    int answer = num1 + num2;
+    printf("What is %d + %d?", num1, num2);
+    int guess = scanf("Your answer: ");
+    if (guess == answer) {
+      printf("Correct!");
+      correct++;
+    } else {
+      printf("Incorrect:(");
+    }
+  }
+  printf("You answered %d correctly.", correct/round);
   return 0;
 }
