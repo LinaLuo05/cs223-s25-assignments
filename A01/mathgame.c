a6ea2e76821a96f2706b4f35a85e5ed43e544aab
LinaLuo05 <qluo1@haverford.edu>
2025-01-24 13:33:56 -0500

    fix

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,40 +1,43 @@
 /***************************************************
  * mathgame.c
- * Author: Lina Luo
+ * @author: Lina Luo
+ * @version: Jan 24, 2025
  * Implements a math game
  */
 
 #include <stdio.h>
 
 int main() {
+  int round = 0; //variable of how many round does the user want to play
+  int correct = 0;//varaible of the correct answer
+  int guess = 0; //variable of the user's answer
+
   printf("Welcome to Math Game!\n");
-  int round;
   printf("How many rounds do you want to play? ");
   scanf("%d", &round);//takes in the number of rounds the user wants to play
-  while (round < 1) {//if the user enters a number less than 1, the program will ask the user to enter a number greater than 0
+
+  //make sure the round user entered is valid
+  while (round < 1) {
     printf("Please enter a number greater than 0.\n");
-    printf("How many rounds do you want to play? ");
+    printf("How many rounds do you want to play?");
     scanf("%d", &round);
   }
-    int correct = 0;
+
+  //generate random questions for intented rounds
     for (int i = 0; i < round; i++) {
       int num1 = rand()%10;
       int num2 = rand()%10;
       int answer = num1 + num2;
-
-      print("What is %d + %d ?", num1, num2);
-      scanf(%d, &guess);
+      printf("\nWhat is %d + %d ?", num1, num2);
+      scanf("%d", &guess);
+      //keep track of correct rounds
       if (guess == answer) {
-        printf("Correct!\n");
+        printf("Correct!");
         correct++;
       } else {
-        printf("Incorrect:(\n");
+        printf("Incorrect:(");
       }
     }
-    printf("You answered %d correctly.\n", (float)correct/round);
+    printf("You answered %d/%d correctly.", correct, round);
     return 0;
   }
-
-
-
-
