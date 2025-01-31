8a3e852c9c5b73a9fcc16c467e156094d7de4cb6
LinaLuo05 <qluo1@haverford.edu>
2025-01-31 18:23:18 -0500

    cypher

--- A02/cypher.c
+++ A02/cypher.c
@@ -4,23 +4,33 @@
  * This program asks the user for a word and then encodes it using a shift cypher
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
-  char* word;
-  int* shift;
+  char word[100];
+  int* shift = malloc(sizeof(int));
 
   printf("Enter a word:");
-  scanf("%s", &word);
+  scanf("%s", word);
 
   printf("Enter a shift:");
-  scanf("%d", &shift);
-
+  scanf("%d", shift);
+  
   for (int i = 0; i < strlen(word); i++) {
-    word[i] = word[i] + shift;
+    word[i] = word[i] + *shift;
+    if (word[i] > 'z') {
+      word[i] = word[i] - 26;
+    }
+    if (word[i] < 'a'){
+      word[i] = word[i] + 26;
+    }
   }
 
   printf("Your cypher is %s\n", word);
 
+  free(shift);
+
   return 0;
 }
+
