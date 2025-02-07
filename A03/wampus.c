77705581275389c2c73707eb826a6e13edf37679
LinaLuo05 <qluo1@haverford.edu>
2025-02-07 17:16:49 -0500

    1

--- A03/wampus.c
+++ A03/wampus.c
@@ -4,7 +4,17 @@
  * Description: a program that randomly places a Wampus within a NxM grid.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
 
 int main() {
+//get the size of grid
+int row;
+printf("Number of rows:");
+scanf("%d", &row);
+
+int column;
+printf("Number of columns:");
+
   return 0;
 }
