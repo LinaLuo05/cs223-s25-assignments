194d38160087dc15f84083e1b2dfda514d4f38eb
LinaLuo05 <qluo1@haverford.edu>
2025-02-07 17:05:16 -0500

    dynamic snacks

--- A03/dynamic_snacks.c
+++ A03/dynamic_snacks.c
@@ -1,10 +1,41 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Lina Luo
+ * Date: Feb 7, 2025
+ * Description: a program that allows users to add snacks to the snackbar, and show the list of snacks. 
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct snack {
+    char name[100];
+    float cost;
+    int quantity;
+  };
 
 int main() {
+  int numSnacks;
+  printf("Enter a number of snacks:");
+  scanf("%d", &numSnacks);
+  
+  struct snack* snackArray = malloc(numSnacks);
+
+  for (int i = 0; i < numSnacks; i++){
+    printf("Enter a name:");
+    scanf("%s", snackArray[i].name);
+
+    printf("Enter a cost:");
+    scanf("%f", &snackArray[i].cost);
+
+    printf("Enter a quantity:");
+    scanf("%d", &snackArray[i].quantity);
+  }
+
+  printf("Welcome to Dynamic Donna's Snack Bar.\n");
+  for (int i = 0; i < numSnacks; i++) {
+    printf("%d) %s\t cost: $%.2f\t quantity: %d\n", i, snackArray[i].name, snackArray[i].cost, snackArray[i].quantity);
+  }
+  
+
+  free(snackArray);
   return 0;
 }
