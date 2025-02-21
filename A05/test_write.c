449ad829be43e56369212497376441af4e20c183
LinaLuo05 <qluo1@haverford.edu>
2025-02-20 20:20:27 -0500

    write

--- A05/test_write.c
+++ A05/test_write.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Lina Luo
+ * Date: Feb 20, 2005
+ * Description: test case of write_ppm
  ---------------------------------------------*/
 
 #include <stdio.h>
@@ -15,6 +15,22 @@ int main(int argc, char** argv) {
   struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
 
   // test writing the file to test.ppm, reload it, and print the contents
+  write_ppm("test.ppm", pixels, w, h);
+
+
+  int w2, h2;
+  struct ppm_pixel* pixel2 = read_ppm("test.ppm", &w2, &h2);
+  printf("Testing file test.ppm: %d %d\n",w, h);
+  for (int i = 0 ; i < h; i++){
+    for (int j = 0; j < w; j++){
+      printf("(%d, %d, %d) ", pixel2[i*w + j].red, 
+      pixel2[i*w + j].green, pixel2[i*w + j].blue);
+    }
+    printf("\n");
+  }
+
+
   free(pixels);
+  free(pixel2);
   return 0;
 }
