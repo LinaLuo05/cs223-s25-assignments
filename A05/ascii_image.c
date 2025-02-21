6ff67704ea9b0cf1e281b7d4167b3872b293450f
LinaLuo05 <qluo1@haverford.edu>
2025-02-20 21:23:57 -0500

    1

--- A05/ascii_image.c
+++ A05/ascii_image.c
@@ -1,12 +1,15 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Lina Luo
+ * Date: Feb 20, 2025
+ * Description: takes a PPM image as a command line argument and displays it as ASCII Art.
  ---------------------------------------------*/
 #include <stdio.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
 
 int main() {
+  
+
+
   return 0;
 }
