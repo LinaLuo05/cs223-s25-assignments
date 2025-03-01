3f7ebde6a7e578a428437f9c319e8bc33567dcb6
LinaLuo05 <qluo1@haverford.edu>
2025-03-01 16:00:41 -0500

    braces

--- A06/match_braces.c
+++ A06/match_braces.c
@@ -10,43 +10,49 @@
 #include <stdlib.h>
 
 struct node{
+  char c;
   int line;
-  int colomn;
+  int column;
   struct node* next;
 };
 
-void push(int line, int colomn, struct node** head){
+void push(char c, int line, int column, struct node** head){
   struct node* n = malloc(sizeof(struct node));
+  n->c = c;
   n->line = line;
-  n->colomn = colomn;
+  n->column = column;
   n->next = *head;
   *head = n;
 }
 
-struct node pop(struct node* head){
-  struct node target = *head;
-  head = target->next;
-  return target;
+struct node* pop(struct node** head) {
+  if (*head == NULL)
+      return NULL;
+  struct node* top = *head;
+  *head = top->next;
+  return top;
 }
 
 void printList(struct node* head){
   struct node* n = head;
   int i = 0;
   while (n != NULL){
-    printf("%d) Line %d and Colomn %d\n", i, n->line, n->colomn);
+    printf("%d) Line %d and Colomn %d\n", i, n->line, n->column);
     i++;
     n = n->next;
   }
 }
-
-void clear(struct node* head){
-  struct node* current = head;
+/*
+void clear(struct node** head) {
+  struct node* current = *head;
   while (current != NULL) {
-    struct node* next = current->next;
-    free(current);
-    current = next;
+      struct node* next = current->next;
+      free(current);
+      current = next;
   }
+  *head = NULL;
 }
+*/
 
 int main(int argc, char** argv)
 {
@@ -55,7 +61,7 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  FILE* infile = fopen(filename, "r");
+  FILE* infile = fopen(argv[1], "r");
   if (!infile){
     printf("Cannot open file: %s\n", argv[1]);
     return 1;
@@ -63,36 +69,36 @@ int main(int argc, char** argv)
 
   struct node* stack = NULL;
   int line = 1;
-  int colomn = 1;
+  int column = 1;
+  int ch;
   
-  while (fgetc(infile) != NULL){
-    char curr = fgetc(infile);
-    if (curr == '{'){
-      push(line,colomn, stack);
+  while ((ch = fgetc(infile)) != EOF) {
+    char curr = (char) ch;
+    if (curr == '{') {
+      push(curr, line, column, &stack);
+      column++;
+    } else if (curr == '}') {
+      struct node* n = pop(&stack);
+      if (n == NULL) {
+        fprintf(stderr, "Unmatched closing brace at line %d, column %d\n", line, column);
+      } else {
+        printf("Found matching braces: (%d, %d) -> (%d, %d)\n", n->line, n->column, line, column);
+        free(n);
+      }
+      column++;
+    } else if (curr == '\n') {
+      line++;
+      column = 1;
+    } else {
+      column++;
     }
-    elif (curr == '}'){
-      pop(struct);
-
-    }
-    elif (curr = '\n'){
-      line ++;
-      colomn = 0; 
-    }
-    else{
-      colomn ++;
-    }
-    
-
   }
-
-
-
-
-
-
-  
-
-
+  while(stack != NULL){
+    fprintf(stderr, "Unmatched closing brace at line %d, column %d\n", stack->line, stack->column);
+    struct node* n = pop(&stack);
+    free(n);
+  }
+  fclose(infile);
 
   return 0;
 }
