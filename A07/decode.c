9bb9086271b55fad7be15d75426786f28cf19de5
LinaLuo05 <qluo1@haverford.edu>
2025-03-03 18:20:56 -0500

    decode1

--- A07/decode.c
+++ A07/decode.c
@@ -1,7 +1,8 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Lina Luo
+ * Date: March 7, 2025
+ * Description: This program reads in a PPM file (raw, or binary, format),
+ * and then outputs any message that might be stored in the least significant bits of each color.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +12,39 @@ int main(int argc, char** argv) {
   if (argc != 2) {
     printf("usage: decode <file.ppm>\n");
     return 0;
+  }
+  FILE* fp = fopen(argv[1], "r");
+  if (!fp){
+    printf("Cannot open file: %s\n", argv[1]);
+    return 1;
+  }
+  char* filename = argv[1];
+  int w;
+  int h; 
+  struct ppm_pixel* image = read_ppm(filename, &w, &h);
+  
+  printf("Reading %s with width %d and height %d\n", filename, w, h);
+  int total_channels = 3 * w * h;
+  int max_chars = total_channels / 8;
+  printf("Max number of characters in the image: %d\n", max_chars);
+  
+  unsigned char *message = malloc(sizeof(unsigned char)* (max_chars + 1));
+
+  int msg_index = 0;
+  int total_pixels = w * h;
+  for (int i = 0; i < total_pixels; i++){
+    message[msg_index] = (image[i].red & 0x01)+'0';
+    msg_index++;
+    message[msg_index] = (image[i].green & 0x01) +'0';
+    msg_index++;
+    message[msg_index] = (image[i].blue & 0x01)+'0';
+    msg_index++;
+  }
+  message[msg_index] = '\0';
+  printf("%s\n", message);
+
+  for (int i = 0 ; i< 8; i++){
+    
   }
 
   return 0;
--- A07/write_ppm.c
+++ A07/write_ppm.c
@@ -1,18 +1,28 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Lina Luo
+ * Date: feb 20
+ * Description: writes to a file test.ppm by calling your function
  ---------------------------------------------*/
-#include "write_ppm.h"
-#include <stdio.h>
-#include <string.h>
-
-// implement *one* (but do not remove the other one)!
-
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
-
-}
-
-void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
-
-}
+ #include "write_ppm.h"
+ #include <stdio.h>
+ #include <string.h>
+ 
+ // implement *one* (but do not remove the other one)!
+ 
+ void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+     FILE* outfile = fopen(filename, "wb");
+     fprintf(outfile,"P6\n#comments\n%d %d 255 ", w, h);
+     for (int i = 0; i < h; i++) {
+         for (int j = 0; j < w; j++) {
+             fwrite(&pixels[i * w + j], sizeof(struct ppm_pixel), 1, outfile);
+         }
+     }    
+     fclose(outfile);
+ 
+ }
+ 
+ 
+ void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+ 
+ }
+ 
