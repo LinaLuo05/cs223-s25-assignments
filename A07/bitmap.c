9c02829e73830c7a89acd731a154f6f8da4a4ec0
LinaLuo05 <qluo1@haverford.edu>
2025-03-03 16:58:53 -0500

    bitmap

--- A07/bitmap.c
+++ A07/bitmap.c
@@ -1,7 +1,9 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Lina Luo
+ * Date: March 7, 2025
+ * Description:  This program that reads in a 
+ * single 64-bit unsigned integer (e.g. unsigned long) 
+ * and outputs it as an 8x8 1bpp sprite.
  ---------------------------------------------*/
 #include <stdio.h>
 
@@ -9,7 +11,20 @@ int main() {
   unsigned long img;
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
+  
+  unsigned long mask = 0x1ul << 63;
+  for (int j = 0; j < 8; j++){
+    for (int i =0; i < 8; i++){
+      if (img & mask){
+        printf("@ ");
+      }
+      else{
+        printf("  ");
+      }
+      mask = mask >> 1;    
+    }
+    printf("\n");
+  }
 
-  // todo: your code here
   return 0;
 }
