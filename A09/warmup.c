027a82c146552d02e804e36e1715ea3bbce286d6
LinaLuo05 <qluo1@haverford.edu>
2025-03-25 15:59:45 -0400

    warmup

--- A09/warmup.c
+++ A09/warmup.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
+
 
 int main() {
+  pid_t pid, mypid; 
+  printf("%d] A0\n", getpid());
+  pid = fork();
+  if(pid){
+    printf("%d] B0\n", getpid());
+    mypid = fork();
+    if(mypid){
+      printf("%d] C0\n", getpid());
+      printf("%d] Bye\n", getpid());
+    }
+    else{
+      printf("%d] C1\n", getpid());
+      printf("%d] Bye\n", getpid());
+    }
+  }
+  else{
+    printf("%d] B1\n", getpid());
+    printf("%d] Bye\n", getpid());
+  }
+
   return 0;
 }
