ec11c87d7d0dabf681836fa12acd75a4ff723a6b
LinaLuo05 <qluo1@haverford.edu>
2025-04-22 17:00:13 -0400

    1

--- J06/shot.c
+++ J06/shot.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+
+void handler(int sig) {
+  if (sig == SIGINT) {
+    printf("Help! I've been shot!");
+    fflush(stdout);
+    exit(0);
+  }
+}
 int main()
 {
+  signal(SIGINT, handler);
+  while (1) {
+  }
   return 0;
 }
